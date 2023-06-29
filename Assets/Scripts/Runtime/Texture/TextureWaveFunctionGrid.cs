01c1a721986c7d0775c2329cb69b00c87342ee81
Nico Auerswald <auersw.nico@gmail.com>
2023-06-29 17:19:20 +0200

    setup scene and wavefunctiongrid as the view

---
+++ Assets/Scripts/Runtime/Texture/TextureWaveFunctionGrid.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runtime.Texture {
+    public class TextureWaveFunctionGrid : MonoBehaviour {
+        [SerializeField] Texture2D inputTexture;
+        [SerializeField] int outputSizeX;
+        [SerializeField] int outputSizeY;
+
+        TextureInput input;
+        List<ColorRule> rules;
+        List<ColorState> states;
+
+        protected void Start() {
+            input = new TextureInput(inputTexture);
+            rules = input.Read(out states);
+
+            Debug.Log("Possible States:");
+            foreach (var state in states) {
+                Debug.Log(state.Color);
+            }
+
+            Debug.Log("Rules:");
+            foreach (var rule in rules) {
+                Debug.Log(rule.FirstColor + " + " + rule.SecondColor);
+            }
+        }
+    }
+}
