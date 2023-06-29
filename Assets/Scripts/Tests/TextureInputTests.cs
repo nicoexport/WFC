52bbe90771721d8406d79cc742c48596a50435ad
Nico Auerswald <auersw.nico@gmail.com>
2023-06-29 14:51:23 +0200

    setup texture input test for reading a texture

---
+++ Assets/Scripts/Tests/TextureInputTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace Tests {
+    public class TextureInputTests {
+        const string PATH = "Assets/Input/";
+
+        [Test]
+        [TestCase("Sprite-0001.png", 2, 2)]
+        public void T00_TextureInput_Read(string textureName, int expectedNumberOfRules,
+            int expectedNumberOfPossibleStates) {
+            var texture = AssetDatabase.LoadAssetAtPath<Texture2D>($"{PATH}{textureName}");
+            Assert.IsTrue(texture);
+            var input = new TextureInput(texture);
+            var rules = input.Read(out var possibleStates);
+
+            Assert.IsTrue(rules.Length == expectedNumberOfRules);
+            Assert.IsTrue(possibleStates.Length == expectedNumberOfPossibleStates);
+        }
+    }
+}
