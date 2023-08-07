67013ecb7831395cb50a5c7422b1cf74c101c338
Nico Auerswald <auersw.nico@gmail.com>
2023-08-07 16:00:12 +0200

    add test category

---
+++ Assets/Scripts/SimpleTiles/Runtime/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Runtime.CompilerServices;
+using WFC.SimpleTiles;
+
+[assembly: InternalsVisibleTo(AssemblyInfo.NAMESPACE_EDITOR)] 
+[assembly: InternalsVisibleTo(AssemblyInfo.NAMESPACE_TESTS)]
+
+namespace WFC.SimpleTiles {
+    static class AssemblyInfo {
+        public const string NAMESPACE_RUNTIME = "WFC.SimpleTiles";
+        public const string NAMESPACE_EDITOR = "WFC.SimpleTiles.Editor";
+        public const string NAMESPACE_TESTS = "WFC.SimpleTiles.Tests";
+    }
+}
---
+++ Assets/Scripts/SimpleTiles/Tests/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using WFC.SimpleTiles;
+
+[assembly: TestMustExpectAllLogs]
+[assembly: Category(AssemblyInfo.NAMESPACE_RUNTIME)]
