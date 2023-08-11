241bef6f98938f4db4c7fc4fd6d5944608515e8a
Nico Auerswald <auersw.nico@gmail.com>
2023-08-11 15:30:50 +0200

    test: start using valuesource for rules

--- Assets/Scripts/SimpleTiles/Tests/InputTextureReaderTests.cs
+++ Assets/Scripts/SimpleTiles/Tests/InputTextureReaderTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using UnityEditor;
@@ -7,6 +8,23 @@ namespace WFC.SimpleTiles.Tests {
     [TestFixture(TestOf = typeof(InputTextureReader))]
     public class InputTextureReaderTests {
         const string PATH = "Assets/Scripts/SimpleTiles/TestAssets/";
+
+        public record TextureRulesPair(string path, IEnumerable<(Color, Color, Direction)> rules) {
+            
+        }
+
+        public static IEnumerable<(Color, Color, Direction)> Rules01 {
+            get {
+                yield return new(Color.red, Color.green, Direction.Right);
+                yield return new(Color.green, Color.red, Direction.Left);
+            }
+        }
+
+        public static IEnumerable<TextureRulesPair> RulesPairs {
+            get {
+                yield return new("Sprite-0001.png", Rules01);
+            }
+        }
 
         [Test]
         [TestCase("Sprite-0001.png")]
@@ -19,17 +37,23 @@ namespace WFC.SimpleTiles.Tests {
             var texture = AssetDatabase.LoadAssetAtPath<Texture2D>($"{PATH}{textureName}");
             Assert.IsTrue(texture);
         }
-        
-        
+
         [Test]
         [TestCase("Sprite-0001.png", 2)]
-        public void T01_InputTextureReader_LoadTextures(string textureName, int expectedNumberOfRules) {
+        public void T01_InputTextureReader_NumberOfRules(string textureName, int expectedNumberOfRules) {
             var texture = AssetDatabase.LoadAssetAtPath<Texture2D>($"{PATH}{textureName}");
             Assert.IsTrue(texture);
-            var sut = new InputTextureReader(texture);
 
+            var sut = new InputTextureReader(texture);
             var rules = sut.GenerateRules();
             Assert.AreEqual(expectedNumberOfRules, rules.ToList().Count);
+        }
+
+        [Test]
+        public void T02_GivenTexture_WhenGenerateRules_ThenReturnAllRules(
+            [ValueSource(nameof(RulesPairs))] TextureRulesPair rulePairs) {
+            var texture = AssetDatabase.LoadAssetAtPath<Texture2D>($"{PATH}{rulePairs.path}");
+
         }
     }
 }
