955e71005cd4c79b75416b64bdaf6b31bf9d4680
Nico Auerswald <auersw.nico@gmail.com>
2023-08-11 17:31:04 +0200

    feat: start setting up wavegrid

---
+++ Assets/Scripts/SimpleTiles/Runtime/WaveGrid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace WFC.SimpleTiles {
+    public class WaveGrid {
+        public WaveGrid(Texture2D input, int outputSizeX, int outputSizeY) {
+            this.input = input;
+            this.outputSizeX = outputSizeX;
+            this.outputSizeY = outputSizeY;
+            cellAmount = this.outputSizeX * this.outputSizeY;
+        }
+
+        readonly Texture2D input;
+        readonly int outputSizeX;
+        readonly int outputSizeY;
+        readonly int cellAmount; 
+
+        IEnumerable<(Color, int)> weightedStates;
+        IEnumerable<(Color, Color, Direction)> rules;
+        IEnumerable<Color>[] wave;
+
+        public void Setup() {
+            var reader = new InputTextureReader(input);
+            weightedStates = reader.GetWeightedStates();
+            rules = reader.GetRules();
+            
+            wave = new IEnumerable<Color>[cellAmount];
+        }
+    }
+}
