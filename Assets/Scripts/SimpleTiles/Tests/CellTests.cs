c96ddccb136731c013663e0bdfd2414b2d3b3671
Nico Auerswald <auersw.nico@gmail.com>
2023-08-25 22:37:38 +0200

    test: setup test for matching cells

--- Assets/Scripts/SimpleTiles/Tests/CellTests.cs
+++ Assets/Scripts/SimpleTiles/Tests/CellTests.cs
@@ -1,11 +1,31 @@
 ﻿using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Windows;
 using WFC.SimpleTiles;
 
 namespace WFC.SimpleTiles.Tests {
     [TestFixture(TestOf = typeof(SimpleWaveGrid))]
     sealed class CellTests {
+
+        const string PATH = "Assets/Scripts/SimpleTiles/TestAssets/";
+
+        Texture2D texture;
+        InputTextureReader reader;
+        IEnumerable<(Color, Color, Direction)> rules;
+        IEnumerable<Color> states;
+
+
+        [SetUp]
+        public void SetUp() {
+            texture = AssetDatabase.LoadAssetAtPath<Texture2D>($"{PATH}Sprite07.png");
+            reader = new InputTextureReader(texture);
+            rules = reader.GetRules();
+            states = reader.GetWeightedStates().Select(x => x.Item1);
+        }
 
         [Test]
         public void T00_WhenCollapse_IsCollapsedIsTrue() {
@@ -18,6 +38,16 @@ namespace WFC.SimpleTiles.Tests {
             sut.Collapse(); 
 
             Assert.That(sut.IsCollapsed, Is.True);
+        }
+
+        [Test]
+        public void TO1_Cell_WhenMatch_OnlyValidStates() {
+            var sut = new Cell(states, rules);
+            var otherCell = new Cell(states.Take(3), rules);
+
+            sut.Match(otherCell, Direction.Left);
+
+            Assert.That(sut.Match(otherCell, Direction.Left), Is.True);
         }
     }
 }
