a2c6b9d12c7e824c2e8ef66d2c32574983fc6112
Nico Auerswald <auersw.nico@gmail.com>
2023-08-25 22:10:43 +0200

    test: setup cell test class

---
+++ Assets/Scripts/SimpleTiles/Tests/CellTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using WFC.SimpleTiles;
+
+namespace WFC.SimpleTiles.Tests {
+    [TestFixture(TestOf = typeof(SimpleWaveGrid))]
+    sealed class CellTests {
+
+        [Test]
+        public void T00_WhenCollapse_IsCollapsedIsTrue() {
+            IEnumerable<Color> colors = new[] { Color.red, Color.green, Color.blue };
+            IEnumerable<(Color, Color, Direction)> rules = new[] {
+                (Color.red, Color.blue, Direction.Down)
+            };
+
+            var sut = new Cell(colors, rules);
+            sut.Collapse(); 
+
+            Assert.That(sut.IsCollapsed, Is.True);
+        }
+    }
+}
