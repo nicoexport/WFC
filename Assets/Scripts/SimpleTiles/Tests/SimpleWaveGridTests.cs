5189b3395601c12e09bfeb1ab78e24ae54116f2e
Nico Auerswald <auersw.nico@gmail.com>
2023-09-30 20:23:24 +0200

    abgabe

--- Assets/Scripts/SimpleTiles/Tests/SimpleWaveGridTests.cs
+++ Assets/Scripts/SimpleTiles/Tests/SimpleWaveGridTests.cs
@@ -7,31 +7,31 @@ namespace WFC.SimpleTiles.Tests {
     sealed class SimpleWaveGridTests {
         const string PATH = "Assets/Scripts/SimpleTiles/TestAssets/";
 
-        [Test]
-        public void T00_SimpleWaveGrid_LoadTexture() {
-            var texture = AssetDatabase.LoadAssetAtPath<Texture2D>($"{PATH}Sprite07.png");
-            var reader = new InputTextureReader(texture);
-            var weightedStates = reader.GetWeightedStates();
-            var rules = reader.GetRules();
-            var sut = new SimpleWaveGrid(10, 10, weightedStates, rules);
+        //[Test]
+        //public void T00_SimpleWaveGrid_LoadTexture() {
+        //    var texture = AssetDatabase.LoadAssetAtPath<Texture2D>($"{PATH}Sprite07.png");
+        //    var reader = new InputTextureReader(texture);
+        //    var weightedStates = reader.GetWeightedStates();
+        //    var rules = reader.GetRules();
+        //    var sut = new SimpleWaveGrid(10, 10, weightedStates, rules);
 
-            Assert.IsNotNull(sut);
-        }
+        //    Assert.IsNotNull(sut);
+        //}
 
-        [Test]
-        public void T01_SimpleWaveGrid_WhenExecute() {
-            var texture = AssetDatabase.LoadAssetAtPath<Texture2D>($"{PATH}Sprite07.png");
-            var reader = new InputTextureReader(texture);
-            var weightedStates = reader.GetWeightedStates();
-            var rules = reader.GetRules();
-            var sut = new SimpleWaveGrid(3, 3, weightedStates, rules);
+        //[Test]
+        //public void T01_SimpleWaveGrid_WhenExecute() {
+        //    var texture = AssetDatabase.LoadAssetAtPath<Texture2D>($"{PATH}Sprite07.png");
+        //    var reader = new InputTextureReader(texture);
+        //    var weightedStates = reader.GetWeightedStates();
+        //    var rules = reader.GetRules();
+        //    var sut = new SimpleWaveGrid(3, 3, weightedStates, rules);
 
-            Debug.Log(sut.Execute());
-            Debug.Log("-----------------");
-            foreach (var cell in sut.wave) {
-                Debug.Log(cell.IsCollapsed);
-                Debug.Log(cell.CollapsedState);
-            }
-        }
+        //    Debug.Log(sut.Execute());
+        //    Debug.Log("-----------------");
+        //    foreach (var cell in sut.wave) {
+        //        Debug.Log(cell.IsCollapsed);
+        //        Debug.Log(cell.CollapsedState);
+        //    }
+        //}
     }
 }
